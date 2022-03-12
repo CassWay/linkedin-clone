52ca9ce941bd5d30d5a4548501fd238de0ba203e
Wayne Cassity <webdev.cassity@gmail.com>
2022-03-12 02:26:55 -0800

    completed Header, still need to tweek, --work on Sidebar

--- src/Header.js
+++ src/Header.js
@@ -2,16 +2,18 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import HeaderOption from "./HeaderOption";
 
 function Header() {
 	return (
 		<div className="header">
-			<h1>This is the Header</h1>
-
 			<div className="header__left">
-				<div className="logo">
-					<LinkedInIcon fontSize="large" color="primary" />
-				</div>
+				<LinkedInIcon fontSize="large" color="primary" />
 
 				<div className="header__search">
 					<SearchIcon />
@@ -19,7 +21,17 @@ function Header() {
 				</div>
 			</div>
 
-			<div className="header__right"></div>
+			<div className="header__right">
+				<HeaderOption Icon={HomeIcon} title="Home" />
+				<HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+				<HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+				<HeaderOption Icon={ChatIcon} title="Messaging" />
+				<HeaderOption Icon={NotificationsIcon} title="Notifications" />
+				<HeaderOption
+					avatar="https://images.unsplash.com/photo-1614741118887-7a4ee193a5fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+					title="me"
+				/>
+			</div>
 		</div>
 	);
 }
