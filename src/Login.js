5835d824702d18d0eef7d4818b6b8ee5a0efd982
Wayne Cassity <webdev.cassity@gmail.com>
2022-03-14 02:11:46 -0700

    finished Login pg, tweaked styles, --work on Auth & link Firebase

--- src/Login.js
+++ src/Login.js
@@ -2,9 +2,31 @@ import React from "react";
 import "./Login.css";
 
 function Login() {
+	const loginToApp = () => {};
+	const register = () => {};
+
 	return (
 		<div className="login">
-			<h1>You are Not Logged-In </h1>
+			<img
+				src="https://www.edigitalagency.com.au/wp-content/uploads/Linkedin-logo-png.png"
+				alt=""
+			/>
+
+			<form>
+				<input placeholder="Full Name (required if registering)" type="text" />
+				<input placeholder="profile pic URL (optional)" type="text" />
+				<input placeholder="email" type="email" />
+				<input placeholder="password" type="password" />
+				<button type="submit" onClick={loginToApp}>
+					Sign in
+				</button>
+			</form>
+			<p>
+				Not a member? {""}
+				<span className="login__register" onClick={register}>
+					Register Now
+				</span>
+			</p>
 		</div>
 	);
 }
