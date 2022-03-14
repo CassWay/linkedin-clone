5c5f3b58cf4b514d2714645eea4dccd7e8c6b4ab
Wayne Cassity <webdev.cassity@gmail.com>
2022-03-14 03:59:10 -0700

    functions -login, bug with controlled - uncontrolled values -React-Dom(view docs)--next bugfix

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "./features/userSlice";
+import { auth } from "./firebase";
 import "./Login.css";
 
 function Login() {
-	const loginToApp = () => {};
-	const register = () => {};
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [name, setName] = useState("");
+	const [profilePic, setProfilePic] = useState("");
+	const dispatch = useDispatch();
+
+	const loginToApp = (e) => {
+		e.preventDefault();
+	};
+	const register = () => {
+		if (!name) {
+			return alert("Please enter your Full Name");
+		}
+
+		auth
+			.createUserWithEmailAndPassword(email, password)
+			.then((userAuth) => {
+				userAuth.user
+					.updateProfile({
+						displayName: name,
+						photoURL: profilePic,
+					})
+					.then(() => {
+						dispatch(
+							login({
+								email: userAuth.user.email,
+								uid: userAuth.user.uid,
+								displayName: name,
+								photoUrl: profilePic,
+							})
+						);
+					});
+			})
+			.catch((error) => alert(error));
+	};
 
 	return (
 		<div className="login">
@@ -13,10 +49,30 @@ function Login() {
 			/>
 
 			<form>
-				<input placeholder="Full Name (required if registering)" type="text" />
-				<input placeholder="profile pic URL (optional)" type="text" />
-				<input placeholder="email" type="email" />
-				<input placeholder="password" type="password" />
+				<input
+					value={name}
+					onChange={(e) => setName(e.target.value.value)}
+					placeholder="Full Name (required if registering)"
+					type="text"
+				/>
+				<input
+					value={profilePic}
+					onChange={(e) => setProfilePic(e.target.value.value)}
+					placeholder="profile pic URL (optional)"
+					type="text"
+				/>
+				<input
+					value={email}
+					onChange={(e) => setEmail(e.target.value.value)}
+					placeholder="email"
+					type="email"
+				/>
+				<input
+					value={password}
+					onChange={(e) => setPassword(e.target.value.value)}
+					placeholder="password"
+					type="password"
+				/>
 				<button type="submit" onClick={loginToApp}>
 					Sign in
 				</button>
